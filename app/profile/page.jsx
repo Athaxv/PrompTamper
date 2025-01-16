5763b3b55280642941101aff832debbae53a8f24
AtharvGaur <atharava112@gmail.com>
2025-01-16 11:16:13 +0530

    created users profile

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,9 +1,22 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import Profile from '@/components/Profile'
 
 const MyProfile = () => {
+  const { data: session} = useSession()
+  const [posts, setPosts] = useState([])
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`api/users/${session?.user.id}/posts`)
+      const data = await response.json();
+      setPosts(data);
+    }
+    console.log(posts)
+    fetchPosts()
+  }, [])
   const handleEdit = () => {
 
   }
@@ -14,7 +27,7 @@ const MyProfile = () => {
     <Profile
       name="My"
       desc="Welcome to your Personalized Profile Page"
-      data={[]}
+      data={posts}
       handleEdit={handleEdit}
       hadleDelete={handleDelete}
     />
